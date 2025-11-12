9030f8cd84c631fe840f85a81a00b30b1be97a0b
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-11-11 22:19:31 -0300

    Apreendendo fazendo get e post para o backend
    Apreendendo fazendo get e post para o backend

--- 2025.11.04_InicioNode/index.js
+++ 2025.11.04_InicioNode/index.js
@@ -1,13 +1,43 @@
 const express = require("express")
 const app = express()
-app.get('/', function (req, res) {
-    res.send('Hello Word')
+
+const herois = ['Mulher Maravilha', 'Superman', 'Batman']
+
+app.get('/herois', function (req, res) {
+    res.send(herois)
 })
-app.get('/oi', function (req, res) {
-    res.send('Óla, Mundo')
+
+app.get('/herois/:id', function (req, res) {
+    const id = req.params.id
+    res.send(herois[id - 1])
 })
+
+
+app.use(express.json());
+
+app.post('/herois', (req, res) => {
+    const nomeHerois = req.body.nome;
+    herois.push(nomeHerois);
+    res.send(nomeHerois)
+})
+
+
+// Forma de fazer um POSTl
+// let nome = [];
+// app.post('/inserir', (req, res) => {
+//     const novoNome = req.body;
+//     novoNome.id = nome.length + 1;
+//     nome.push(novoNome);
+//     res.status(201).json({
+//         mensagem: '✅ Usuário inserido com sucesso!',
+//         nome: novoNome
+//     });
+// });
+
+// Rota para listar os usuários já inseridos
+app.get('/listar', (req, res) => {
+    res.json(usuarios);
+});
+
 
 app.listen(3000)
-
-
-
