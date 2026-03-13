ab2b42bcec7f6dc9048824471d053d462206332b
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2026-03-13 19:19:28 -0300

    Criação de pasta, exclusão e renomear

---
+++ 2026.03.12_CriacaoPastas/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Rodando");
+});
+
+app.listen(process.env.PORTA, () => {
+  console.log(`Servidor rodando em http://localhost:${process.env.PORTA}`);
+});
